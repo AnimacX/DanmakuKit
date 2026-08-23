ed26d064bf20703e0a43967ed45fdf2930ffa59c
saddit <issue@anix.app>
2026-08-23 22:10:03 +0800

    fix: pause

--- Sources/DanmakuKit/Classes/Core/DanmakuTrack.swift
+++ Sources/DanmakuKit/Classes/Core/DanmakuTrack.swift
@@ -391,7 +391,16 @@ class DanmakuVerticalTrack: NSObject, DanmakuTrack, CAAnimationDelegate {
         guard let findCell = cells.first(where: { (c) -> Bool in
             return c.model?.isEqual(to: danmaku) ?? false
         }) else { return false }
+        #if os(macOS)
+        if let layer = findCell.layer {
+            let pausedTime = layer.timeOffset
+            layer.speed = 1.0
+            layer.timeOffset = 0.0
+            layer.beginTime = clock.currentTime - pausedTime
+        }
+        #else
         addAnimation(to: findCell)
+        #endif
         return true
     }
 
@@ -410,7 +419,11 @@ class DanmakuVerticalTrack: NSObject, DanmakuTrack, CAAnimationDelegate {
             return c.model?.isEqual(to: danmaku) ?? false
         }) else { return false }
         #if os(macOS)
-        findCell.layer?.removeAllAnimations()
+        if let layer = findCell.layer {
+            let pausedTime = layer.convertTime(clock.currentTime, from: nil)
+            layer.speed = 0.0
+            layer.timeOffset = pausedTime
+        }
         #else
         findCell.layer.removeAllAnimations()
         #endif
--- Sources/DanmakuKit/Classes/Core/DanmakuView.swift
+++ Sources/DanmakuKit/Classes/Core/DanmakuView.swift
@@ -543,6 +543,9 @@ public extension DanmakuView {
     
     func pause() {
         guard status != .pause else { return }
+#if os(macOS)
+        stopCurrentHovered()
+#endif
         floatingTracks.forEach {
             $0.pause()
         }
